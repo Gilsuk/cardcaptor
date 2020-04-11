b1ddee0f83670f8ae810743b373978c71a2077c5
gilsuk <devgilsuk@gmail.com>
2020-04-11 23:17:05 +0900

    add features reading configures from json

---
+++ lib/conf.go
@@ -0,0 +1,35 @@
+package lib
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+/*
+Conf is
+*/
+type Conf struct {
+	ClientID     string `json:"client_id"`
+	ClientSecret string `json:"client_secret"`
+}
+
+/*
+NewConf is parsed values from conf.json
+*/
+func NewConf() (conf Conf) {
+	jsonFile, err := os.Open("./conf.json")
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	defer jsonFile.Close()
+
+	bytes, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(bytes, &conf)
+
+	return
+}
