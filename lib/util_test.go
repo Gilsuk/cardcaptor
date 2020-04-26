a108360f646a49d3474c47a4710d44c074a46607
gilsuk <devgilsuk@gmail.com>
2020-04-26 14:14:29 +0900

    change resource path

--- lib/util_test.go
+++ lib/util_test.go
@@ -6,21 +6,21 @@ import (
 )
 
 func Test파일이없으면false를반환(t *testing.T) {
-	isExist := lib.IsFileExist("./resources/test/not-existed-conf.json")
+	isExist := lib.IsFileExist("./non-existing-file")
 	if isExist {
 		t.Fail()
 	}
 }
 
 func Test파일이있으면true를반환(t *testing.T) {
-	isExist := lib.IsFileExist("./resources/test/testconf.json")
+	isExist := lib.IsFileExist("./util.go")
 	if !isExist {
 		t.Fail()
 	}
 }
 
 func Test경로가존재하지않으면false를반환(t *testing.T) {
-	isExist := lib.IsFileExist("./resources/non-directory/conf.json")
+	isExist := lib.IsFileExist("./resources/non-directory/")
 	if isExist {
 		t.Fail()
 	}
