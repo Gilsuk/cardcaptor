e5b914a20704184b8e171230f7b28bc46e56d19c
gilsuk <devgilsuk@gmail.com>
2020-04-29 15:50:52 +0900

    add Http GET request to cards

--- lib/blizzard.go
+++ lib/blizzard.go
@@ -1,12 +1,10 @@
 package lib
 
 import (
-	"bytes"
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"os"
 	"strconv"
 )
 
@@ -148,17 +146,6 @@ type HSJsonCard struct {
 type HSJsonResp []HSJsonCard
 
 /*
-Export is
-*/
-func (c *Card) Export() {
-	path := "./res/card"
-	os.MkdirAll(path, os.ModePerm)
-
-	bytes, _ := json.Marshal(*c)
-	ioutil.WriteFile(path+"/"+c.Slug, bytes, os.ModePerm)
-}
-
-/*
 HasNext is
 */
 func (c *CardResp) HasNext() bool {
@@ -171,64 +158,30 @@ func (c *CardResp) HasNext() bool {
 /*
 Next is
 */
-func (c *CardResp) Next() CardResp {
-	return CardResp{}
-}
-
-/*
-RequestToken is
-*/
-func RequestToken() (respJSON TokenResp) {
-	conf := NewConf()
-	req := TokenReq{GrantType: "client_credentials", ClientID: conf.ClientID, ClientSecret: conf.ClientSecret}
-	reqJSON, _ := json.Marshal(req)
-	reqBytes := bytes.NewBuffer(reqJSON)
-
-	resp, err := http.Post("https://apac.battle.net/oauth/token", "application/json", reqBytes)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	respBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	json.Unmarshal(respBytes, &respJSON)
-	return
+func (c *CardResp) Next(accessToken string) (CardResp, error) {
+	return RequestCards(c.Page+1, accessToken)
 }
 
 /*
 RequestCards is
 */
-func RequestCards(page int) (cardResp CardResp) {
-	conf := NewConf()
+func RequestCards(page int, accessToken string) (cardResp CardResp, err error) {
 	base, _ := url.Parse("https://kr.api.blizzard.com")
 	base.Path += "hearthstone/cards"
 	params := url.Values{}
 	params.Add("locale", "ko_KR")
 	params.Add("page", strconv.Itoa(page))
-	params.Add("access_token", conf.AccessToken)
+	params.Add("access_token", accessToken)
 	base.RawQuery = params.Encode()
 
-	println(base.String())
 	resp, err := http.Get(base.String())
 	if err != nil {
-		println("Request fail")
 		return
 	}
-
 	defer resp.Body.Close()
+
 	if respBytes, err := ioutil.ReadAll(resp.Body); err == nil {
 		json.Unmarshal(respBytes, &cardResp)
-	}
-
-	println(cardResp.CardCount)
-	println(cardResp.Cards[0].Img)
-
-	for _, card := range cardResp.Cards {
-		card.Export()
 	}
 
 	return
